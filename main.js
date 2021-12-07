1db0b71f1fbe6f5c919f965bd8b15dc5c6887241
alexander-yuen <79123774+alexander-yuen@users.noreply.github.com>
2021-12-06 20:34:26 -0500

    Add files via upload
    HAMBURGER

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 img = "";
+status = "";
 
 function setup(){
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
 }
 
-function preload(){
-    img = loadImage('dog_cat.jpg');
-}
+
 
 function draw(){
     image(img, 0, 0, 640, 420);
@@ -17,6 +17,24 @@ function draw(){
     stroke("#FF0000");
     rect(30, 60, 450, 350);
 
+    fill("#FF0000");
+    text("Cat", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
+
 }
 
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+}
 //superb!!! :)
